1c6470db60b31c4ada76b656e6a28794bb94fbd3
David González Martínez <me@davidgonmar.me>
2024-05-05 22:32:42 +0200

    feat: add multi-query-attention

--- tests/test_attn.py
+++ tests/test_attn.py
@@ -1,9 +1,9 @@
 import unittest
 import torch
-from my_transformers import MultiHeadAttention
+from my_transformers import MultiHeadAttention, MultiQueryAttention
 
 
-class TestPositionWiseFeedForward(unittest.TestCase):
+class TestMultiHeadAttention(unittest.TestCase):
     def test_forward(self):
         d_model = 512
         num_heads = 8
@@ -13,6 +13,17 @@ class TestPositionWiseFeedForward(unittest.TestCase):
         out = attn(x, x, x)
         self.assertEqual(out.size(), (32, 100, d_model))
 
+
+class TestMultiQueryAttention(unittest.TestCase):
+    def test_forward(self):
+        d_model = 512
+        num_heads = 8
+        d_k = d_v = d_model // num_heads
+        attn = MultiQueryAttention(num_heads, d_model, d_k, d_v, causal=False)
+        x = torch.randn(32, 100, d_model)
+        out = attn(x, x, x)
+        self.assertEqual(out.size(), (32, 100, d_model))
+    
 
 if __name__ == "__main__":
     unittest.main()
